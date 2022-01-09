187c89827916690526c78b3d9a74058993fbce28
partyka <partyka95@icloud.com>
2022-01-09 18:17:30 +0100

    improve

--- src/app/server.ts
+++ src/app/server.ts
@@ -4,7 +4,7 @@ import { router } from './router';
 export const server = http.createServer((req, res) => {
   try {
     router.handle(req, res);
-  } catch (err) {
-    console.log('globalny error: ', err);
+  } catch (error) {
+    console.error(error);
   }
 });

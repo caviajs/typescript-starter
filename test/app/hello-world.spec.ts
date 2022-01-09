2ed0cc331e3f80a447ae78336b4808f11c71c452
partyka <partyka95@icloud.com>
2022-01-09 18:16:36 +0100

    improve

--- test/app/hello-world.spec.ts
+++ test/app/hello-world.spec.ts
@@ -4,9 +4,9 @@ import { server } from '../../src/app/server';
 describe('GET /hello-world', () => {
   it('should return Hello World', async () => {
     const result = await TestBed
-      .request(server, { url: 'hello-world', responseType: 'text' });
+      .get(server, 'hello-world', { responseType: 'text' });
 
     expect(result.body).toBe('Hello World');
-    expect(result.status).toBe(200);
+    expect(result.statusCode).toBe(200);
   });
 });

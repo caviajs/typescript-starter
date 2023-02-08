4e223e15cd5353031d3da3f5e282c148d0f61d5c
Paweł Partyka <partyka95@icloud.com>
2023-02-08 21:55:36 +0100

    build: bump dependencies

--- src/interceptors/cors-interceptor.ts
+++ src/interceptors/cors-interceptor.ts
@@ -1,12 +1,8 @@
 import { HttpCors } from '@caviajs/http-cors';
 import { Interceptor } from '@caviajs/http-router';
 
-export const CorsInterceptor: Interceptor = (request, response, next) => {
-  HttpCors.setup(request, response, {
-    'Access-Control-Allow-Origin': 'http://localhost:4200',
-    'Access-Control-Allow-Methods': ['DELETE', 'GET', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT'],
-    'Access-Control-Allow-Credentials': true,
-  });
-
-  return next.handle();
-};
+export const CorsInterceptor: Interceptor = HttpCors.setup({
+  'Access-Control-Allow-Origin': 'http://localhost:4200',
+  'Access-Control-Allow-Methods': ['DELETE', 'GET', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT'],
+  'Access-Control-Allow-Credentials': true,
+});

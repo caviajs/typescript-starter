86e39b9a965cd5be746e638d51185e33276c3527
Paweł Partyka <partyka95@icloud.com>
2023-11-22 19:11:12 +0100

    schema

--- app/routes/guinea-pig-create-route.ts
+++ app/routes/guinea-pig-create-route.ts
@@ -8,8 +8,18 @@ const REQUEST_BODY_SCHEMA = z
   })
   .strict();
 
+const RESPONSE_BODY_SCHEMA_201 = z
+  .object({
+    data: z
+      .object({
+        id: z.string().uuid(),
+      })
+      .strict(),
+  })
+  .strict();
+
 export const GuineaPigCreateRoute: Route = {
-  handler: async (request, response): Promise<void> => {
+  handler: async (request, response): Promise<z.infer<typeof RESPONSE_BODY_SCHEMA_201>> => {
     const body = await HttpParser
       .requestBody(request, REQUEST_BODY_SCHEMA, 'json');
 
@@ -17,6 +27,12 @@ export const GuineaPigCreateRoute: Route = {
     // ...
 
     response.statusCode = 201;
+
+    return await HttpParser.responseBody(request, RESPONSE_BODY_SCHEMA_201, {
+      data: {
+        id: 'e931c62f-11bf-434f-bb40-facfef7f5b20',
+      },
+    });
   },
   metadata: {
     swagger: {
@@ -33,6 +49,11 @@ export const GuineaPigCreateRoute: Route = {
       responses: {
         '201': {
           description: 'Guinea pig created',
+          content: {
+            'application/json': {
+              schema: RESPONSE_BODY_SCHEMA_201,
+            },
+          },
         },
         '400': {
           description: '',
